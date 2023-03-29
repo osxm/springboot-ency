1f5f03d05c7257c1692063655d40522b16da083d
Chen XM <oscar999@mail.ustc.edu.cn>
2023-03-30 06:59:22 +0800

    ok

---
+++ src/main/java/com/osxm/springbootency/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+/**  
+* @Title: WebSecurityConfig.java
+* @Package com.osxm.springbootency.config
+* @Description: TODO
+* @author XM
+* @date 2023年3月30日 上午6:44:27
+* @Copyright: 2023
+* @version V1.0  
+*/
+package com.osxm.springbootency.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.authorizeRequests().
+				antMatchers("/**").anonymous().and().csrf().disable().build();
+	}
+}
---
+++ src/main/java/com/osxm/springbootency/extjs/ParamController.java
@@ -0,0 +1,33 @@
+/**  
+* @Title: ParamController.java
+* @Package com.osxm.springbootency.extjs
+* @Description: TODO
+* @author XM
+* @date 2023年3月27日 下午9:58:15
+* @Copyright: 2023
+* @version V1.0  
+*/
+package com.osxm.springbootency.extjs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.OPTIONS,RequestMethod.PUT})
+public class ParamController {
+	
+
+	@RequestMapping(value="/params/urlparam",produces="application/json")
+	public Map<String,String> urlParam(@RequestParam String param1){
+		Map<String,String> map = new HashMap<String,String>();
+		String msg = "传递的参数 param1="+param1;
+		map.put("msg",msg);
+		return map; 
+	}
+}
