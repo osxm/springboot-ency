ad56cc219cbf9cc4e7d284bd7dad2cafc6e17563
Chen XM <oscar999@mail.ustc.edu.cn>
2023-05-20 07:53:58 +0800

    ok

--- src/test/java/com/osxm/springbootency/redis/RedisTests.java
+++ src/test/java/com/osxm/springbootency/redis/RedisTests.java
@@ -11,12 +11,15 @@ package com.osxm.springbootency.redis;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.osxm.springbootency.topics.redis.RedisClient;
 
 @SpringBootTest
+//@RunWith(SpringRunner.class)
 public class RedisTests {
 
 	@Autowired
